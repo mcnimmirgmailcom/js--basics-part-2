d81991400e3cec947e79001e999f0d4214e622e7
mcnimmir@gmail.com <mcnimmir@gmail.com>
2025-10-27 20:11:56 +0100

    Practice

--- src/methods/array-methods.js
+++ src/methods/array-methods.js
@@ -1,25 +1,50 @@
 // lös uppgifterna med olika arraymetoder. titta på förväntat testresultat om du behöver ledtrådar
 
-export const reverseArray = (array) => {};
-
-export function removeAppleFromArray() {
-  const array = ["apple", "banana", "cherry"];
-}
-
-export function removeFiveFroremmArray() {
-  const array = [1, 2, 3, 4, 5];
-}
-
-export const getFirstElement = (array) => {};
-
-export const getLastElement = (array) => {};
-
-export const removeMiddleItem = () => {
-  const array = [1, 2, 3, 4, 5];
+export const reverseArray = (array) => {
+  const copy = [...array];
+  return copy.reverse();
 };
+let numOfReverseArray = reverseArray.length;
+console.log(reverseArray(["Dog", "Cat", "Meow"]));
+console.log(numOfReverseArray);
 
-// en blandning av sträng- och array-metoder
-export const reverseString = (string) => {};
+// export function removeAppleFromArray() {
+//   // Använder .splice() metod, använda för att ta bort eller lägga till element i en array
+//   const array = ["apple", "banana", "cherry"];
+//   array.splice(0, 1);
+//   return array;
+// }
+// console.log(removeAppleFromArray());
 
-// kan kräva higher order functions
-export const sumNumbers = (array) => {};
+// export function removeFiveFroremmArray() {
+//   // Använder .splice() metod, använda för att ta bort eller lägga till element i en array
+//   const array = [1, 2, 3, 4, 5];
+//   array.splice(4, 1);
+//   return array;
+// }
+// console.log(removeFiveFroremmArray());
+
+// export const getFirstElement = (array) => {
+//   const copy = [...array];
+//   return copy.shift();
+// };
+// console.log(getFirstElement(["mi", "bombo", "clart"]));
+
+// export const getLastElement = (array) => {
+//   const copy = [...array];
+//   return copy.pop();
+// };
+// console.log(getLastElement(["Sonic", "Shadow", "Silver"]));
+
+// export const removeMiddleItem = () => {
+//   const array = [1, 2, 3, 4, 5];
+//   array.splice(2, 1);
+//   return array;
+// };
+// console.log(removeMiddleItem());
+
+// // en blandning av sträng- och array-metoder
+// export const reverseString = (string) => {};
+
+// // kan kräva higher order functions
+// export const sumNumbers = (array) => {};
--- src/methods/object-methods.js
+++ src/methods/object-methods.js
@@ -1,10 +1,18 @@
 // lös uppgifterna med olika objektmetoder. titta på förväntat testresultat om du behöver ledtrådar
 
-export const getKeys = (object) => {};
+export const getKeys = (object) => {
+  return Object.keys(object);
+};
 
-export const getValues = (object) => {};
+export const getValues = (object) => {
+  return Object.values(object);
+};
+console.log(getValues({ Hero: "Sonic", Ugh: "Mario", Nobody: "Zelda" }));
 
-export const getEntries = (object) => {};
+export const getEntries = (object) => {
+  return Object.entries(object);
+};
+console.log(getEntries({ Cha: "Sonic", Age: "15", Hobby: "Running" }));
 
 export const fromEntriesToObject = (entriesArray) => {};
 
